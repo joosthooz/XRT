c5a9b8a7f62db5e15b1876f20da2b5d0b803e788
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-07-12 15:28:33 +0200

    Updated dockerfile

--- docker/build_xrt.sh
+++ docker/build_xrt.sh
@@ -1,3 +1,3 @@
 docker build . -t xrtbuild
-docker run --name xrtbuild xrtbuild #I don't understand, but it seems this only needs to be executed once
+#docker run --name xrtbuild xrtbuild #I don't understand, but it seems this only needs to be executed once
 docker cp xrtbuild:/work/XRT/build/Release ./
